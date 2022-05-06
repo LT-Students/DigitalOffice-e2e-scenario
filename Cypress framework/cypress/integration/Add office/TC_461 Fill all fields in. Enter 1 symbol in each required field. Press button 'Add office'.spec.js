624ee40d63d58c9450899ebbdd84276cf14d57f3
Alyona_Martynova <19martynovaap@gmail.com>
2022-05-06 19:09:26 +0300

    new

---
+++ Cypress framework/cypress/integration/Add office/TC_461 Fill all fields in. Enter 1 symbol in each required field. Press button 'Add office'.spec.js
@@ -0,0 +1,14 @@
+describe("Log in", function(){
+	it("Sing in", function(){
+		cy.visit("https://dev.ltdo.xyz/auth/login")
+		cy.get('input[type="email"]').type ('Alyona')
+		cy.get('input[type="password"]').type ('5WZWqx#NPK')
+		cy.get('button[type="submit"]').click ()
+		cy.visit("https://dev.ltdo.xyz/admin/dashboard")
+		cy.get('img[alt="add-project-icon"]').eq(4).click ()
+		cy.get('input[type="text"]').eq(0).type ('П')
+		cy.get('input[type="text"]').eq(1).type ('С')
+		cy.get('button[type="submit"]').click ()
+
+	})
+})
