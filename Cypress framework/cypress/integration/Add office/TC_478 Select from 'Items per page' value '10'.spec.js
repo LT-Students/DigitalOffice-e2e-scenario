68340aa4825b77b3b50413aa10bf01edba5fa6d6
Alyona_Martynova <19martynovaap@gmail.com>
2022-05-16 20:26:16 +0300

    change

---
+++ Cypress framework/cypress/integration/Add office/TC_478 Select from 'Items per page' value '10'.spec.js
@@ -0,0 +1,14 @@
+describe("Log in", function(){
+	it("Sing in", function(){
+		cy.visit("https://dev.ltdo.xyz/auth/login")
+		cy.get('input[type="email"]').type ('Alyona')
+		cy.get('input[type="password"]').type ('5WZWqx#NPK')
+		cy.get('button[type="submit"]').click ()
+		cy.visit("https://dev.ltdo.xyz/admin/dashboard")
+		cy.get('img[alt="add-project-icon"]').eq(8).click ()
+		cy.get('mat-select[role="combobox"]').eq(1).click ()
+		cy.get('mat-option[role="option"]').eq(0).click ()
+
+		
+	})
+})
