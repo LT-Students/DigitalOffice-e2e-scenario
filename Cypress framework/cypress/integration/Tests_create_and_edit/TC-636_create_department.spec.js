84ccca7392ad14c173fec385abcbe7de00799e22
Vika <victory120595@yandex.ru>
2022-05-18 14:51:16 +0300

    re

--- Cypress framework/cypress/integration/Tests_create_and_edit/TC-636_create_department.spec.js
+++ Cypress framework/cypress/integration/Tests_create_and_edit/TC-636_create_department.spec.js
@@ -2,10 +2,11 @@ describe("log in", ()  => {
   it("sing in", () => {
     cy.visit('https://dev.ltdo.xyz/auth/login');
     cy.log('Переход на страницу авторизации');
-    cy.log('Корпоративный e-mail или логин');
+  
     cy.get('input[type="email"]').type("admin");
     cy.log('Пароль');
     cy.get('input[type="password"]').type("ltstudents");
+    
     cy.log('Клик по кнопке "Войти"');
     cy.get('button[type="submit"]').click(); 
     cy.get('a[href*="/admin"]').click();
@@ -27,4 +28,3 @@ describe("log in", ()  => {
   
     });
   });
-//})
